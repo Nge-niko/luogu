527c733c01e5c8f6dde060b2353d572a9813586d
Nge-niko <2370304878@qq.com>
2024-11-15 18:48:35 +0800

    新提交

--- P5710.cpp
+++ P5710.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int a=0,b=0,c=0,d=0,x=0;
+int a=0,b=0,c=0,d=0,x;
 int main()
 {
     cin >> x;
@@ -8,6 +8,6 @@ int main()
     if(x%2==0||x>=4&&x<=12||x%2==0&&x>=4&&x<=12)b=1;
     if(x%2==0&&x<=4&&x>=12||x%2==1&&x>=4&&x<=12)c=1;
     if(x%2==1&&x<=4&&x>=12)d=1;
-    cout << a << " " << b << " " << c << " " << d;
+    cout << a << " " << b << " " << c << " " << d << endl;
     return 0;
 }
